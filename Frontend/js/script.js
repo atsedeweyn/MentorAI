77ddb2a43f107a86c0fd535fa512a4d05ffc69f4
akc7523 <akc7523@rit.edu>
2024-11-02 18:08:01 -0400

    Added some css and javascript to style page and make input and button more responsive. Still need to style page more.

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,4 +1,7 @@
-document.getElementById('start_button').onclick = function {
-    const url = document.getElementById('yt_link').value; 
-    window.location.href = "";
+document.getElementById('start_button').onclick = function() {
+    const url = document.getElementById('yt_link').value;
+    if (!url) {
+        return false; 
+    }
+    window.location.href = "home.html";
 }
