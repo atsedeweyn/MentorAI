bae7f2ea50f61ba5e75131cee8c38860c9f47093
akc7523 <akc7523@rit.edu>
2024-11-03 08:19:43 -0500

    Styled button and changed page to look nicer. Button is disabled unless given input. Still have to make page more interactive and style more.

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -1,8 +1,18 @@
-document.getElementById('start_button').onclick = function(event) {
-    event.preventDefault();
+document.getElementById('start_button').addEventListener('click', (event) => {
+    event.preventDefault(); 
     const url = document.getElementById('yt_link').value.trim(); 
     if (url === "") {
         return false; 
     }
-    window.location.href = "home.html";
-}
+    window.location.href = "home.html"; 
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const input = document.getElementById('yt_link'); 
+    const button = document.getElementById('start_button');
+    input.addEventListener('input', () => {
+        if (input.value.trim() !== "") {
+            button.disabled = false; 
+        }
+    })
+})
