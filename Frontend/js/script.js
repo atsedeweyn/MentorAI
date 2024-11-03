9bbc5d228ede6c816e3e206187ebc35d8defbdab
akc7523 <akc7523@rit.edu>
2024-11-03 01:16:53 -0500

    Fixed button to go next to search bar. Changed styling and created more appropriate wording and naming.

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -2,7 +2,6 @@ document.getElementById('start_button').onclick = function(event) {
     event.preventDefault();
     const url = document.getElementById('yt_link').value.trim(); 
     if (url === "") {
-        alert("Please enter a channel name");
         return false; 
     }
     window.location.href = "home.html";
